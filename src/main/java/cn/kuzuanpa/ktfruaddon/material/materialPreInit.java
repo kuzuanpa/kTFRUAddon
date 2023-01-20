bc54c4cd64983ffedf009cf39771a261b0c36899
kuzuanpa <misaka16384@outlook.com>
2023-01-20 17:10:27 +0800

    insert license in code

--- src/main/java/cn/kuzuanpa/ktfruaddon/material/materialPreInit.java
+++ src/main/java/cn/kuzuanpa/ktfruaddon/material/materialPreInit.java
@@ -1,3 +1,11 @@
+/**
+ * This class was created by <kuzuanpa>. It is distributed as
+ * part of the kTFRUAddon Mod. Get the Source Code in github:
+ * https://github.com/kuzuanpa/kTFRUAddon
+ * 
+ * kTFRUAddon is Open Source and distributed under the
+ * LGPLv3 License: https://www.gnu.org/licenses/lgpl-3.0.txt
+ */
 package cn.kuzuanpa.ktfruaddon.material;
 
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
