39a25d3993fa3bbacea00db4572616d58302b166
kuzuanpa <misaka16384@outlook.com>
2023-03-29 00:08:40 +0800

    allow insert&extract items using hands,trying to add render to show whether there is a node

--- src/main/java/cn/kuzuanpa/ktfruaddon/item/item/itemComputer.java
+++ src/main/java/cn/kuzuanpa/ktfruaddon/item/item/itemComputer.java
@@ -13,37 +13,42 @@ package cn.kuzuanpa.ktfruaddon.item.item;
 import cn.kuzuanpa.ktfruaddon.item.util.ItemList;
 import gregapi.item.CreativeTab;
 import gregapi.item.multiitem.MultiItemRandom;
+import net.minecraft.item.Item;
 
 import static cn.kuzuanpa.ktfruaddon.ktfruaddon.MOD_ID;
 
 public class itemComputer extends MultiItemRandom {
     public itemComputer() {
     super(MOD_ID, "ktfru.item.it.computer");
-    setCreativeTab(new CreativeTab(itemIT.unlocalizedName, "kTFRUAddon: IT", this, (short) 9999));
+    setCreativeTab(new CreativeTab(getUnlocalizedName(), "kTFRUAddon: Computers", this, (short) 9999));
 }
-
+    public static long getComputePowerFromID(int id){
+        if (id > ComputePower.length) return 0;
+        return ComputePower[id];
+    }
+    private static final long[] ComputePower ={1,4,12,80,135};
     @Override
     public void addItems() {
-        ItemList.ComputeNodeTF386.set(addItem(10300,"TF386 Compute Node","Ex"));
-        ItemList.ComputeNodeTF586.set(addItem(10301,"TF586 Compute Node","Ex"));
-        ItemList.ComputeNodeAE300.set(addItem(10302,"AE-300 Compute Node","Ex"));
-        ItemList.ComputeNodeAE740.set(addItem(10303,"AE-740 Compute Node","Ex"));
-        ItemList.ComputeNodeGT200.set(addItem(10304,"GT-200 Compute Node","Ex"));
-        ItemList.ComputeNodeGT3490.set(addItem(10305,"GT-3490 Compute Node","Ex"));
-        ItemList.ComputeNodeGT6799.set(addItem(10306,"GT-6799 Compute Node","Ex"));
-        ItemList.ComputeNodeGT9890.set(addItem(10307,"GT-9890 Compute Node","Ex"));
-        ItemList.ComputeNodeGT13586.set(addItem(10308,"GT-13586 Compute Node","Ex"));
-        ItemList.ComputeNodeIF2.set(addItem(10309,"IF2 Compute Node","Ex"));
-        ItemList.ComputeNodeIF7.set(addItem(10310,"IF7 Compute Node","Ex"));
-        ItemList.ComputeNodeTF386e.set(addItem(10311,"Compute Node TF386e","Ex"));
-        ItemList.ComputeNodeTF586e.set(addItem(10312,"Compute Node TF586e","Ex"));
-        ItemList.ComputeNodeAE300s.set(addItem(10313,"Compute Node AE300s","Ex"));
-        ItemList.ComputeNodeAE740s.set(addItem(10314,"Compute Node AE740s","Ex"));
-        ItemList.ComputeNodeGT200s.set(addItem(10315,"Compute Node GT200s","Ex"));
-        ItemList.ComputeNodeGT3490s.set(addItem(10316,"Compute Node GT3490s","Ex"));
-        ItemList.ComputeNodeGT6799s.set(addItem(10317,"Compute Node GT6799s","Ex"));
-        ItemList.ComputeNodeGT9890s.set(addItem(10318,"Compute Node GT9890s","Ex"));
-        ItemList.ComputeNodeGT13586s.set(addItem(10319,"Compute Node GT13586s","Ex"));
+        ItemList.ComputeNodeTF386.set(addItem(0,"TF386 Compute Node","Ex"));
+        ItemList.ComputeNodeTF586.set(addItem(1,"TF586 Compute Node","Ex"));
+        ItemList.ComputeNodeAE300.set(addItem(2,"AE-300 Compute Node","Ex"));
+        ItemList.ComputeNodeAE740.set(addItem(3,"AE-740 Compute Node","Ex"));
+        ItemList.ComputeNodeGT200.set(addItem(4,"GT-200 Compute Node","Ex"));
+        ItemList.ComputeNodeGT3490.set(addItem(5,"GT-3490 Compute Node","Ex"));
+        ItemList.ComputeNodeGT6799.set(addItem(6,"GT-6799 Compute Node","Ex"));
+        ItemList.ComputeNodeGT9890.set(addItem(7,"GT-9890 Compute Node","Ex"));
+        ItemList.ComputeNodeGT13586.set(addItem(8,"GT-13586 Compute Node","Ex"));
+        ItemList.ComputeNodeIF2.set(addItem(9,"IF2 Compute Node","Ex"));
+        ItemList.ComputeNodeIF7.set(addItem(10,"IF7 Compute Node","Ex"));
+        ItemList.ComputeNodeTF386e.set(addItem(11,"Compute Node TF386e","Ex"));
+        ItemList.ComputeNodeTF586e.set(addItem(12,"Compute Node TF586e","Ex"));
+        ItemList.ComputeNodeAE300s.set(addItem(13,"Compute Node AE300s","Ex"));
+        ItemList.ComputeNodeAE740s.set(addItem(14,"Compute Node AE740s","Ex"));
+        ItemList.ComputeNodeGT200s.set(addItem(15,"Compute Node GT200s","Ex"));
+        ItemList.ComputeNodeGT3490s.set(addItem(16,"Compute Node GT3490s","Ex"));
+        ItemList.ComputeNodeGT6799s.set(addItem(17,"Compute Node GT6799s","Ex"));
+        ItemList.ComputeNodeGT9890s.set(addItem(18,"Compute Node GT9890s","Ex"));
+        ItemList.ComputeNodeGT13586s.set(addItem(19,"Compute Node GT13586s","Ex"));
 
     }
 }
