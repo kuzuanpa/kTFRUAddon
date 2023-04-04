52cbb6d70109e20dec04cfeb31d61454f652f617
kuzuanpa <misaka16384@outlook.com>
2023-04-04 21:03:05 +0800

    first CPU crafting workflow

--- src/main/java/cn/kuzuanpa/ktfruaddon/recipe/recipe/ComputerBuilding.java
+++ src/main/java/cn/kuzuanpa/ktfruaddon/recipe/recipe/ComputerBuilding.java
@@ -22,21 +22,40 @@ public class ComputerBuilding {
                 , ItemList.fakeItemSiliconPlateCoated.get(1)
                 , ItemList.fakeItemSiliconPlateSoftBaked.get(1)
         ), ST.array(
-                ItemList.fakeItemCPUWafer.get(1)
-                , ItemList.fakeItemSiliconCPUWaferDeveloped.get(1)
-                , ItemList.fakeItemSiliconCPUWaferHardBaked.get(1)
-                , ItemList.fakeItemSiliconCPUWaferDoped.get(1)
-                , ItemList.fakeItemSiliconCPUWaferChecked.get(1)
-                , ItemList.fakeItemCPUDie.get(1)
+                ItemList.fakeItemWafer.get(1)
+                , ItemList.fakeItemSiliconWaferDeveloped.get(1)
+                , ItemList.fakeItemSiliconWaferHardBaked.get(1)
+                , ItemList.fakeItemSiliconWaferDoped.get(1)
+                , ItemList.fakeItemSiliconWaferChecked.get(1)
+                , ItemList.fakeItemDie.get(1)
         ), null, ZL_LONG, FL.array(FL.Sap_Rainbow.make(250)),ZL_FS, 0, 0, 0);
 
-        recipeManager.ElectronicsDesigner.addRecipeX(T,32,36000, ST.array(IL.Circuit_Good.get(0),
-                gRegistry.getItem(10102,0),gRegistry.getItem(10112,0), OP.plate.mat(MT.SiO2,4)),
-                FL.array(ZL_FS),FL.array(ZL_FS),ItemList.CPUPhotomask200um.get(1));
+        //addRecipeX(T,GUt,Duration, ST.array(),FL.array(),FL.array(), );
 
-        recipeManager.LaserCutter.addRecipeX(T,96,2000, ST.array(OP.bouleGt.mat(MT.Si, 1)),FL.array(ZL_FS),FL.array(ZL_FS), ItemList.SiliconPlate8inchTier1.get(1));
+        recipeManager.ElectronicsDesigner.addRecipeX(T,32,36000, ST.array(IL.Circuit_Good.get(0), gRegistry.getItem(10102,0),gRegistry.getItem(10112,0), OP.plate.mat(MT.SiO2,4)), FL.array(ZL_FS),FL.array(ZL_FS),ItemList.CPUPhotomask200um.get(1));
+
+        recipeManager.LaserCutter.addRecipeX(T,96,2000, ST.array(OP.bouleGt.mat(MT.Si, 1)),FL.array(ZL_FS),FL.array(ZL_FS), ItemList.SiliconPlate8inchTier1.get(12));
+
+        recipeManager.UltraCleanBath.addRecipeX(T,0,800, ST.array(),FL.array(),FL.array(), ItemList.SiliconPlate8inchCleanedTier1.get(1));
+
+        recipeManager.UltraCleanFurnace.addRecipeX(T,110,1200, ST.array(),FL.array(),FL.array(), ItemList.SiliconPlate8inchOxidizedTier1.get(1));
+
+        recipeManager.WaferCoater.addRecipeX(T,64,200, ST.array(),FL.array(),FL.array(), ItemList.SiliconPlate8inchCoatedTier1.get(1));
+
+        recipeManager.UltraCleanDryer.addRecipeX(T,0,400, ST.array(),FL.array(),FL.array(), ItemList.SiliconPlate8inchSoftBakedTier1.get(1));
 
         recipeManager.MaskAligner.addRecipeX(T, 256, 4000, ST.array(ItemList.SiliconPlate8inchSoftBakedTier1.get(1),ItemList.CPUPhotomask200um.get(0)), FL.array(ZL_FS), FL.array(ZL_FS), ItemList.CPUWafer200um.get(1));
         recipeManager.MaskAligner.addRecipeX(T, 512, 4000, ST.array(ItemList.SiliconPlate8inchSoftBakedTier2.get(1),ItemList.CPUPhotomask72um.get(0)), FL.array(ZL_FS), FL.array(ZL_FS), ItemList.CPUWafer72um.get(1));
+
+        recipeManager.UltraCleanBath.addRecipeX(T,0,400, ST.array(),FL.array(),FL.array(), ItemList.CPUWafer200umDeveloped.get(1));
+
+        recipeManager.UltraCleanFurnace.addRecipeX(T,0,400, ST.array(),FL.array(),FL.array(), ItemList.CPUWafer200umHardBaked.get(1));
+
+        recipeManager.UltraCleanBath.addRecipeX(T,0,400, ST.array(),FL.array(),FL.array(), ItemList.CPUWafer200umDoped.get(1));
+
+        recipeManager.WaferTester.addRecipeX(T,0,400, ST.array(),FL.array(),FL.array(), ItemList.CPUWafer200umChecked.get(1));
+
+        recipeManager.LaserCutter.addRecipeX(T,0,400,new long[]{4000,1200}, ST.array(),FL.array(),FL.array(), ItemList.CPUDieTF3386.get(37),ItemList.CPUDieTF3386.get(22));
+
     }
 }
