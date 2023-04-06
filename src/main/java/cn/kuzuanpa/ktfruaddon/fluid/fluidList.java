7c62289848a76b21a2b49685217584e204901da7
kuzuanpa <misaka16384@outlook.com>
2023-04-06 17:16:31 +0800

    add multiple choices registering fluid

--- src/main/java/cn/kuzuanpa/ktfruaddon/fluid/fluidList.java
+++ src/main/java/cn/kuzuanpa/ktfruaddon/fluid/fluidList.java
@@ -20,12 +20,31 @@ public enum fluidList {
     testfluid
     ;
     public Fluid fluid;
+    public void register(String name, String localizedName, OreDictMaterial material, int state) {
+        fluid = FL.create(name,localizedName,material,state);
+    }
+    public void register(String name, String localizedName, OreDictMaterial material, int state, long amountPerUnit) {
+        fluid = FL.create(name,localizedName,material,state,amountPerUnit,277);
+    }
+    public void register(String name, String localizedName, OreDictMaterial material, int state, long amountPerUnit, long temp) {
+        fluid = FL.create(name,localizedName,material,state,amountPerUnit,temp);
+    }
+    public void register(String name, String localizedName, OreDictMaterial material, int state, long amountPerUnit, long temp, int density) {
+        fluid = FL.create(name,localizedName,material,state,amountPerUnit,temp);
+        fluid.setDensity(density);
+    }
+    public void register(String name, String localizedName, OreDictMaterial material, int state, long amountPerUnit, long temp, int density, int viscosity) {
+        fluid = FL.create(name,localizedName,material,state,amountPerUnit,temp);
+        fluid.setViscosity(viscosity);
+        fluid.setDensity(density);
+    }
     public void register(String name, String localizedName, OreDictMaterial material, int state, long amountPerUnit, long temp, int density, int viscosity, int luminosity) {
         fluid = FL.create(name,localizedName,material,state,amountPerUnit,temp);
         fluid.setLuminosity(luminosity);
         fluid.setViscosity(viscosity);
         fluid.setDensity(density);
     }
+
 
 
     public Fluid get() {
