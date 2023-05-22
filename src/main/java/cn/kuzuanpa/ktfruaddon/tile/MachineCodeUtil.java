436be55f1a3abf815f6081edad98a35ff8d79098
kuzuanpa <misaka16384@outlook.com>
2023-05-23 01:45:59 +0800

    Most Circuit workflow;code util machine;balance oil process

---
+++ src/main/java/cn/kuzuanpa/ktfruaddon/tile/MachineCodeUtil.java
@@ -0,0 +1,38 @@
+/*
+ * This class was created by <kuzuanpa>. It is distributed as
+ * part of the kTFRUAddon Mod. Get the Source Code in github:
+ * https://github.com/kuzuanpa/kTFRUAddon
+ *
+ * kTFRUAddon is Open Source and distributed under the
+ * LGPLv3 License: https://www.gnu.org/licenses/lgpl-3.0.txt
+ *
+ */
+
+package cn.kuzuanpa.ktfruaddon.tile;
+
+import cn.kuzuanpa.ktfruaddon.code.CodeTranslate;
+import cpw.mods.fml.common.FMLLog;
+import gregapi.tileentity.machines.MultiTileEntityBasicMachine;
+import net.minecraft.entity.player.EntityPlayer;
+import org.apache.logging.log4j.Level;
+
+import static gregapi.data.CS.ERR;
+import static gregapi.data.CS.T;
+
+public class MachineCodeUtil extends MultiTileEntityBasicMachine {
+@Override
+public boolean onBlockActivated3(EntityPlayer aPlayer, byte aSide, float aHitX, float aHitY, float aHitZ) {
+    if (isServerSide()) {
+        openGUI(aPlayer, aSide);
+        try {
+            for (int i=0;i<this.ACCESSIBLE_SLOTS.length;i++) FMLLog.log(Level.FATAL,CodeTranslate.itemToCode(slot(i)));
+        }catch (Throwable e) {e.printStackTrace(ERR);}
+    }
+    return T;
+}
+@Override
+    public String getTileEntityName() {
+        return "ktfru.multitileentity.machine.code";
+    }
+
+}
