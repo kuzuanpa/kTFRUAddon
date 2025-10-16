715eb58829a6bd6211b942f4800a7da0735cf354
kuzuanpa <misaka16384@outlook.com>
2025-10-16 18:41:22 +0800

    add researches

--- src/main/java/cn/kuzuanpa/ktfruaddon/research/ResearchTrees.java
+++ src/main/java/cn/kuzuanpa/ktfruaddon/research/ResearchTrees.java
@@ -47,89 +47,96 @@ public class ResearchTrees {
                             .addPrerequisite(c);
 
                     ResearchProject f = new ResearchProject(tree, "基础电路设计", "利用计算器进一步改进电路，你认为你离真正的自动化控制不远了", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                            .setPos(340, 70)
                             .addPrerequisite(c).addPrerequisite(d);
 
-                    ResearchProject g = new ResearchProject(tree, "电弧处理", "你的记忆中总能见到电弧，但如何利用它而不损坏材料，需要你进一步研究", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject g = new ResearchProject(tree, "电弧处理", "你的记忆中总能见到电弧，但如何利用它而不损坏材料，需要你进一步研究", Items.paper, 0, 6)
+                            .setPos(340, 140)
                             .addPrerequisite(f);
 
-                    ResearchProject h = new ResearchProject(tree, "高压容器", "许多处理需要高压环境，通过研究常见材料在高压下的变化来制造耐压容器", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject h = new ResearchProject(tree, "高压容器", "许多处理需要高压环境，通过研究常见材料在高压下的变化来制造耐压容器", Items.paper, 0, 7)
+                            .setPos(340, 0)
                             .addPrerequisite(f);
 
-                    ResearchProject i = new ResearchProject(tree, "电磁感应", "你早已听闻电磁感应定律，只需要稍微总结一套方便的规律和程序即可制造大量的实用物品", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject i = new ResearchProject(tree, "电磁感应", "你早已听闻电磁感应定律，只需要稍微总结一套方便的规律和程序即可制造大量的实用物品", Items.paper, 0, 8)
+                            .setPos(440, 0)
                             .addPrerequisite(f);
 
-                    ResearchProject j = new ResearchProject(tree, "电池原理", "利用已知的电解原理进行电力储存", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject j = new ResearchProject(tree, "电池原理", "利用已知的电解原理进行电力储存", Items.paper, 0, 9)
+                            .setPos(440, 140)
                             .addPrerequisite(f);
 
-                    ResearchProject k = new ResearchProject(tree, "半导体制冷", "探索半导体通过电流时的热学行为, 制作半导体制冷器", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject k = new ResearchProject(tree, "半导体制冷", "探索半导体通过电流时的热学行为, 制作半导体制冷器", Items.paper, 0, 10)
+                            .setPos(380, 220)
                             .addPrerequisite(f);
 
-                    ResearchProject l = new ResearchProject(tree, "激光", "研究如何利用电力激发二氧化碳产生激光，激光可将能量集中于极小的一点，非常适合精确加工", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject l = new ResearchProject(tree, "激光", "研究如何利用电力激发二氧化碳产生激光，激光可将能量集中于极小的一点，非常适合精确加工", Items.paper, 0, 11)
+                            .setPos(440, 70)
                             .addPrerequisite(f);
 
-                    ResearchProject m = new ResearchProject(tree, "精确制造", "研究如何利用激光极度精确集中的能量加工物品", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject m = new ResearchProject(tree, "精确制造", "研究如何利用激光极度精确集中的能量加工物品", Items.paper, 0, 12)
+                            .setPos(540, 70)
                             .addPrerequisite(l);
 
-                    ResearchProject n = new ResearchProject(tree, "单晶硅制造", "研究如何制作原子排列规整的单晶硅", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject n = new ResearchProject(tree, "单晶硅制造", "研究如何制作原子排列规整的单晶硅", Items.paper, 0, 13)
+                            .setPos(640, 70)
                             .addPrerequisite(m);
 
-                    ResearchProject o = new ResearchProject(tree, "低级电路设计", "利用性能更好的硅晶片制作算力更强的芯片", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject o = new ResearchProject(tree, "低级电路设计", "利用性能更好的硅晶片制作算力更强的芯片", Items.paper, 0, 14)
+                            .setPos(840, 70)
                             .addPrerequisite(n);
 
-                    ResearchProject p = new ResearchProject(tree, "赛特斯石英应用", "研究异世界特有的独特晶体-赛特斯, 对赛特斯压缩物体的能力进行探索", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject p = new ResearchProject(tree, "赛特斯石英应用", "研究异世界特有的独特晶体-赛特斯, 对赛特斯压缩物体的能力进行探索", Items.paper, 0, 15)
+                            .setPos(840, 140)
                             .addPrerequisite(o);
 
-                    ResearchProject q = new ResearchProject(tree, "智能赛特斯", "研究更为罕见的智金, 对其指导赛特斯流动的能力进行控制和规律利用", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject q = new ResearchProject(tree, "智能赛特斯", "研究更为罕见的智金, 对其指导赛特斯流动的能力进行控制和规律利用", Items.paper, 0, 16)
+                            .setPos(840, 220)
                             .addPrerequisite(p);
 
-                    ResearchProject r = new ResearchProject(tree, "高压发电", "研究如何利用更高算力的电路来控制更高电压的发电机", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject r = new ResearchProject(tree, "高压发电", "研究如何利用更高算力的电路来控制更高电压的发电机", Items.paper, 0, 17)
+                            .setPos(940, 70)
                             .addPrerequisite(o);
 
-                    ResearchProject s = new ResearchProject(tree, "光刻理论", "普通的方法已经达到极限, 你需要研究光刻及相关的设备以制造更精密的芯片", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject s = new ResearchProject(tree, "光刻理论", "普通的方法已经达到极限, 你需要研究光刻及相关的设备以制造更精密的芯片", Items.paper, 0, 18)
+                            .setPos(1040, 70)
                             .addPrerequisite(r);
 
-                    ResearchProject t = new ResearchProject(tree, "计算机系统", "设计一套合理的输入输出标准, 以组装出完整的计算机", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject t = new ResearchProject(tree, "计算机系统", "设计一套合理的输入输出标准, 以组装出完整的计算机", Items.paper, 0, 19)
+                            .setPos(1140, 70)
                             .addPrerequisite(s);
 
-                    ResearchProject u = new ResearchProject(tree, "数控车床", "通过计算机自动化精确控制电机制作数控车床", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject u = new ResearchProject(tree, "数控车床", "通过计算机自动化精确控制电机制作数控车床", Items.paper, 0, 20)
+                            .setPos(1140, 140)
                             .addPrerequisite(t);
 
-                    ResearchProject v = new ResearchProject(tree, "太空基础理论", "火箭与太空探索所需的基本理论", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject v = new ResearchProject(tree, "太空基础理论", "火箭与太空探索所需的基本理论", Items.paper, 0, 21)
+                            .setPos(1140, 0)
                             .addPrerequisite(t);
 
-                    ResearchProject w = new ResearchProject(tree, "火箭基础", "研究火箭的气动外形，引擎推力的改进等基本内容", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject w = new ResearchProject(tree, "火箭基础", "研究火箭的气动外形，引擎推力的改进等基本内容", Items.paper, 0, 22)
+                            .setPos(1240, -30)
                             .addPrerequisite(v);
 
-                    ResearchProject x = new ResearchProject(tree, "空间概论", "研究太空中如何进行航行和维持生命等", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject x = new ResearchProject(tree, "空间概论", "研究太空中如何进行航行和维持生命等", Items.paper, 0, 23)
+                            .setPos(1140, -30)
                             .addPrerequisite(v);
 
-                    ResearchProject y = new ResearchProject(tree, "保护气应用", "研究如何利用保护气制作纯度更高，性能更好的物品", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject y = new ResearchProject(tree, "保护气应用", "研究如何利用保护气制作纯度更高，性能更好的物品", Items.paper, 0, 24)
+                            .setPos(1240, 70)
                             .addPrerequisite(t);
 
-                    ResearchProject z = new ResearchProject(tree, "入门计算机", "利用初代计算机的算力进一步优化电路设计，以提高算力制作下一代计算机", Items.paper, 0, 5)
-                            .setPos(340, 10)
+                    ResearchProject z = new ResearchProject(tree, "入门计算机", "利用初代计算机的算力进一步优化电路设计，以提高算力制作下一代计算机", Items.paper, 0, 25)
+                            .setPos(1340, 70)
                             .addPrerequisite(y);
 
+                    ResearchProject aa = new ResearchProject(tree, "原子结构", "利用物质之间的反应初步确定分子，原子的结构，对不同原子的性质进行研究", Items.paper, 0, 26)
+                            .setPos(1440, 70)
+                            .addPrerequisite(z);
+
+                    ResearchProject ab = new ResearchProject(tree, "原子核理论", "通过研究原子核在各种情况下的状态，提出可能修改原子核的理论", Items.paper, 0, 27)
+                            .setPos(1540, 70)
+                            .addPrerequisite(y);
 
                     return tree;
         }
